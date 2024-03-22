a0088c2495f4d441a6710280a4382bc5a56b5301
Aertic <63727339+Aertic@users.noreply.github.com>
2024-03-22 19:32:17 +0100

    Added uppercase and spacing to blog metadata

--- app/blog/posts/[post]/page.jsx
+++ app/blog/posts/[post]/page.jsx
@@ -2,11 +2,12 @@ import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 
 export async function generateMetadata({ params }) {
-    // read route params
     const id = params.post;
 
+    const meta = await formatString(id);
+
     return {
-        title: `Polar Lab Blog | ${id}`,
+        title: `Polar Lab Blog | ${meta}`,
         description: id,
     };
 }
@@ -21,4 +22,10 @@ export default function Page({ params }) {
             <BlogPost />
         </Suspense>
     );
+}
+
+function formatString(str) {
+    return str.replace(/-/g, ' ').replace(/\b\w/g, function (match) {
+        return match.toUpperCase();
+    });
 }

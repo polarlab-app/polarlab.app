5d61b9db48cb98b00c15f7b662bf1cdd5414e8f4
Aertic <63727339+Aertic@users.noreply.github.com>
2024-05-04 19:57:02 +0200

    Moved sections to their own components + css modules

---
+++ components/main/section.jsx
@@ -0,0 +1,15 @@
+import styles from '@css/main/section.module.css';
+import data from '@data/sections.json';
+
+export default function Section({ children, name }) {
+    return (
+        <div className={styles.sitesection} id={data.ids[name]}>
+            <div className={styles.siteheading}>
+                <h2 className={styles.sectionheader}>{data.headings[name]}</h2>
+                <p className={styles.sectionsubheader}>{data.descriptions[name]}</p>
+            </div>
+
+            <div className={styles.featuregrid}>{children}</div>
+        </div>
+    );
+}

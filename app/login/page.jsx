ff9d3922c66a05fb7f977665adc326a2d4056fd6
Aertic <63727339+Aertic@users.noreply.github.com>
2024-07-04 10:43:19 +0200

    Moved login to css modules, moved login page to login.old, begun work on new login page, deleted empty page preventing compilation

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,84 +1,46 @@
-'use client';
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import login from "@lib/auth/login";
+import Image from "next/image";
 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import '@css/main/login.css';
-
-import login from '@lib/auth/login';
-
-/*export const metadata = {
-    title: 'Polar Lab | Login',
-    description: 'The official Polar Lab login page',
-};*/
+import styles from "@css/main/login.module.css";
 
 export default function Page() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        const loginStatus = await login(username, password);
-
-        if (loginStatus != 'success') {
-            setError(loginStatus);
-            return;
-        }
-
-        setError(null);
-        setSuccess('Login Successful!');
-    };
-
-    return (
-        <div className='main'>
-            <div className='logincontainer'>
-                <form className='loginwrapper' onSubmit={handleSubmit}>
-                    <div className='logintextcontainer'>
-                        <div className='logintexttopcontainer'>
-                            <img
-                                className='logo'
-                                src='https://cdn.polarlab.app/src/img/polarlogo.png'
-                                alt='polarlogo'></img>
-                            <p className='loginheader'>Log In to Polar Lab</p>
-                        </div>
-                        <div className='logininputcontainer'>
-                            {success && <p className='loginsuccess'>{success}</p>}
-                            {error && <p className='loginerror'>{error}</p>}
-                            <p className='inputheader'>Username</p>
-                            <input
-                                type='text'
-                                className='logininput'
-                                placeholder='Username'
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                required></input>
-                            <p className='inputheader'>Password</p>
-                            <input
-                                type='password'
-                                className='logininput'
-                                placeholder='************'
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                required></input>
-                        </div>
-                    </div>
-                    <div className='loginbuttoncontainer'>
-                        <p className='registertext'>
-                            Dont have an account? Register <Link href='/login/register'>here</Link>!
-                        </p>
-                        <button className='loginbutton' type='submit'>
-                            Log In
-                        </button>
-                        <Link
-                            className='discordlogin loginbutton'
-                            href='https://discord.com/api/oauth2/authorize?client_id=1065350226757554237&response_type=code&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Flogin%2Fcallback&scope=identify+guilds'>
-                            Log in with Discord
-                        </Link>
-                    </div>
-                </form>
-            </div>
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <div className={styles.header}>
+            <Image
+              src="https://cdn.polarlab.app/src/img/polarlogo.png"
+              alt="polarlogo"
+              className={styles.logo}
+              height={512}
+              width={512}
+            />
+            <h1 className={styles.heading}>Login To Polar Lab</h1>
+          </div>
+          <button className={`${styles.loginbtn} ${styles.button}`}>
+            Login to Polar Lab
+          </button>
+          <button
+            className={`${styles.discordloginbtn} ${styles.button}`}
+            onClick={() => {
+              window.location.href =
+                "https://discord.com/api/oauth2/authorize?client_id=1065350226757554237&response_type=code&redirect_uri=https%3A%2F%2Flocalhost%3A3000%2Flogin%2Fcallback&scope=identify+guilds";
+            }}
+          >
+            Login With Discord
+          </button>
         </div>
-    );
+        <div className={styles.right}></div>
+      </div>
+    </div>
+  );
 }

f8a94060fd523d5c7cc1c052f92c01d6bad4feb6
Aertic <63727339+Aertic@users.noreply.github.com>
2024-07-27 16:18:50 +0200

    change to async

--- app/blog/posts/[post]/page.jsx
+++ app/blog/posts/[post]/page.jsx
@@ -1,3 +1,4 @@
+'use server';
 import dynamic from 'next/dynamic';
 import { Suspense } from 'react';
 
@@ -12,10 +13,13 @@ export async function generateMetadata({ params }) {
     };
 }
 
-export default function Page({ params }) {
+export default async function Page({ params }) {
     const post = params.post;
 
-    const BlogPost = dynamic(() => import(`@components/blog/posts/${post}.jsx`), { suspense: true });
+    const BlogPost = dynamic(
+        () => import(`@components/blog/posts/${post}.jsx`),
+        { suspense: true }
+    );
 
     return (
         <Suspense fallback={<p>Loading...</p>}>

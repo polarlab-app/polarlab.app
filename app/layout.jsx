7795187bc78bcbf159e5db1337a23cfc6ec82df0
Aertic <63727339+Aertic@users.noreply.github.com>
2024-07-27 15:39:23 +0200

    Add opengraph image, unify border color

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,24 @@ import Footer from './footer.jsx';
 export const metadata = {
     title: 'Polar Lab',
     description: 'Free and Open Source software',
+    keywords: ['Polaris', 'open source', 'Polar Lab'],
+    authors: [{ name: 'Aertic', url: 'https://polarlab.app' }],
+    creator: 'Aetic',
+    publisher: 'Aertic',
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+    openGraph: {
+        images: [
+            {
+                url: 'https://cdn.polarlab.app/src/img/polarlogo.png',
+                width: 1200,
+                height: 630,
+            },
+        ],
+    },
 };
 
 export default function RootLayout({ children }) {

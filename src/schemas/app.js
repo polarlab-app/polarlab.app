cc36fffe4018cd7184c224bb26b78e264c3e2e9f
Aertic <63727339+Aertic@users.noreply.github.com>
2024-07-28 20:32:46 +0200

    Add app schema, some style changes

---
+++ src/schemas/app.js
@@ -0,0 +1,20 @@
+const { Schema, model, models } = require('mongoose');
+
+let appSchema = new Schema(
+    {
+        id: String,
+        pfp: String,
+        name: String,
+        secret: String,
+        redirectUri: String,
+    },
+    {
+        collection: 'apps',
+    }
+);
+
+if (!models.app) {
+    module.exports = model('appSchema', appSchema);
+} else {
+    module.exports = models.app;
+}

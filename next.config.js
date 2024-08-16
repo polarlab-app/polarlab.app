03251cc0f73d31d40c4b2062d07091c1afa219cd
Aertic <63727339+Aertic@users.noreply.github.com>
2024-08-16 22:27:16 +0200

    Add cloudflare to serverActions, remove google from CSP
    Signed-off-by: Aertic <63727339+Aertic@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     experimental: {
         missingSuspenseWithCSRBailout: false,
         serverActions: {
-            allowedOrigins: ['https://polarlab.app', 'polarlab.app', '172.99.0.2:3000'],
+            allowedOrigins: ['https://polarlab.app', 'polarlab.app', '172.99.0.2:3000', 'static.cloudflareinsights.com', 'https://static.cloudflareinsights.com'],
         },
     },
     images: {
@@ -44,7 +44,7 @@ const nextConfig = {
                         key: 'Content-Security-Policy',
                         value: `
                             default-src 'self';
-                            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://static.cloudflareinsights.com;
+                            script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com;
                             style-src 'self' 'unsafe-inline';
                             img-src 'self' data: https://cdn.polarlab.app https://cdn.discordapp.com https://placehold.co;
                             font-src 'self';

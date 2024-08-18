01b81a077d55e30fc345128df495c71902e3f7bb
Aertic <63727339+Aertic@users.noreply.github.com>
2024-08-18 14:10:52 +0200

    Add modal showing; add JSON parsing

--- components/personal/accountDetails/accountDetails.jsx
+++ components/personal/accountDetails/accountDetails.jsx
@@ -3,28 +3,37 @@ import styles from '@css/personal/accountDetails.module.css';
 import Image from 'next/image';
 import findUser from '@/lib/personal/findUser';
 import { useState, useEffect } from 'react';
+import EnterPassword from './enterPassword';
 
 export default function AccountDetails() {
     const [user, setUser] = useState(null);
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showPasswordModal, setShowPasswordModal] = useState(false);
 
     useEffect(() => {
         const fetchUser = async () => {
-            const userAccount = await findUser();
+            const userAccount = JSON.parse(await findUser());
             setUser(userAccount);
             setUsername(userAccount.username);
             setEmail(userAccount.email);
+            console.log(userAccount.username);
         };
         fetchUser();
     }, []);
+
+    const handleSaveChanges = () => {
+        setShowPasswordModal(true);
+    };
 
     return (
         <div className={styles.main}>
             <div className={styles.header}>
                 <h1 className={styles.heading}>Account Details</h1>
-                <button className={styles.button}>Save Changes</button>
+                <button className={styles.button} onClick={handleSaveChanges}>
+                    Save Changes
+                </button>
             </div>
             <div className={styles.account}>
                 <div className={styles.iconContainer}>
@@ -69,6 +78,14 @@ export default function AccountDetails() {
                     </div>
                 </div>
             </div>
+            {showPasswordModal && (
+                <EnterPassword
+                    username={username}
+                    password={password}
+                    email={email}
+                    close={() => setShowPasswordModal(false)}
+                />
+            )}
         </div>
     );
 }

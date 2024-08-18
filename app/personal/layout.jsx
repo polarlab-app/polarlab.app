fd1b2efca40bde8503b455a94db2e67e043f760f
Aertic <63727339+Aertic@users.noreply.github.com>
2024-08-18 14:10:17 +0200

    Remove account checks from layout -> will move to middleware

--- app/personal/layout.jsx
+++ app/personal/layout.jsx
@@ -1,16 +1,10 @@
 'use server';
 import styles from '@css/personal/personal.module.css';
 import NavBar from '@/components/personal/nav';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 import '@css/global/personal.global.css';
 import TopNav from '@/components/personal/topNav';
 
 export default async function PersonalLayout({ children }) {
-    if (!cookies().get('accountToken') || !cookies().get('username')) {
-        redirect('/login');
-    }
-
     return (
         <>
             <TopNav />

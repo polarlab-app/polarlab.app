4e3741a9d48de20c7e48a33247454e71e155a9c9
Aertic <63727339+Aertic@users.noreply.github.com>
2024-08-21 16:34:55 +0200

    Move topBar to other files; move css to global dir

--- components/dashboard/top/topbar.jsx
+++ components/dashboard/top/topbar.jsx
@@ -1,13 +1,20 @@
 import styles from '@css/dashboard/topbar.module.css';
 const { topBar } = require('@data/dashboard.json');
+import DiscardButton from './discardButton';
+import SaveButton from './saveButton';
 
-export default function TopBar({ type, children }) {
+export default function TopBar({ type, showButtons, onDiscard, onSave }) {
     return (
         <div className={styles.dashboardtopbar}>
             <h1 className={styles.topbarheader}>{type.charAt(0).toUpperCase() + type.slice(1)}</h1>
             <hr className={styles.topbardivider}></hr>
             <p className={styles.topbardescription}>{topBar[type]}</p>
-            {children}
+            {showButtons && (
+                <>
+                    <DiscardButton onClick={onDiscard} />
+                    <SaveButton onClick={onSave} />
+                </>
+            )}
         </div>
     );
 }

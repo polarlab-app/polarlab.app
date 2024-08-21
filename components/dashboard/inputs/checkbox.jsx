687300f81122f108f51651757e41e1665d410c3f
Aertic <63727339+Aertic@users.noreply.github.com>
2024-08-21 16:37:47 +0200

    format files

--- components/dashboard/inputs/checkbox.jsx
+++ components/dashboard/inputs/checkbox.jsx
@@ -4,28 +4,26 @@ import styles from '@css/dashboard/checkbox.module.css';
 import { useState } from 'react';
 
 export default function CheckboxInput({ id, value, onChange }) {
-  const [isChecked, setIsChecked] = useState({ value });
+    const [isChecked, setIsChecked] = useState({ value });
 
-  return (
-    <div className={styles.toggleswitchcontainer} onChange={onChange}>
-      <div className={styles.toggleswitchtext}>
-        <p className={styles.toggleswitchheader}>{inputs[id].name}</p>
-        <p className={styles.toggleswitchdescription}>
-          {inputs[id].description}
-        </p>
-      </div>
-      <label className={styles.togglecontainer}>
-        <input
-          id={id}
-          type="checkbox"
-          className={styles.hidden}
-          onChange={() => setIsChecked(!isChecked)}
-          checked={isChecked}
-        ></input>
-        <span className={styles.toggle}>
-          <span className={styles.innertoggle}></span>
-        </span>
-      </label>
-    </div>
-  );
+    return (
+        <div className={styles.toggleswitchcontainer} onChange={onChange}>
+            <div className={styles.toggleswitchtext}>
+                <p className={styles.toggleswitchheader}>{inputs[id].name}</p>
+                <p className={styles.toggleswitchdescription}>{inputs[id].description}</p>
+            </div>
+            <label className={styles.togglecontainer}>
+                <input
+                    id={id}
+                    type='checkbox'
+                    className={styles.hidden}
+                    onChange={() => setIsChecked(!isChecked)}
+                    checked={isChecked}
+                ></input>
+                <span className={styles.toggle}>
+                    <span className={styles.innertoggle}></span>
+                </span>
+            </label>
+        </div>
+    );
 }
--- components/dashboard/inputs/textbox.jsx
+++ components/dashboard/inputs/textbox.jsx
@@ -4,25 +4,25 @@ import styles from '@css/dashboard/textbox.module.css';
 import { useState } from 'react';
 
 export default function TextboxInput({ id, value, onChange, type }) {
-  const [text, setText] = useState(value);
+    const [text, setText] = useState(value);
 
-  return (
-    <div className={styles.textboxcontainer}>
-      <div className={styles.textboxtext}>
-        <p className={styles.textboxheader}>{inputs[id].name}</p>
-        <p className={styles.textboxdescription}>{inputs[id].description}</p>
-      </div>
-      <input
-        id={id}
-        type={type}
-        className={styles.textbox}
-        onChange={(e) => {
-          setText(e.target.value);
-          onChange(e);
-        }}
-        value={text}
-        placeholder={inputs[id].placeholder}
-      />
-    </div>
-  );
+    return (
+        <div className={styles.textboxcontainer}>
+            <div className={styles.textboxtext}>
+                <p className={styles.textboxheader}>{inputs[id].name}</p>
+                <p className={styles.textboxdescription}>{inputs[id].description}</p>
+            </div>
+            <input
+                id={id}
+                type={type}
+                className={styles.textbox}
+                onChange={(e) => {
+                    setText(e.target.value);
+                    onChange(e);
+                }}
+                value={text}
+                placeholder={inputs[id].placeholder}
+            />
+        </div>
+    );
 }

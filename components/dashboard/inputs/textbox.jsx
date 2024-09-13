1b2fbc9388481ef8c243f3ebc123eba5b34e7cf5
Aertic <63727339+Aertic@users.noreply.github.com>
2024-09-13 17:23:14 +0200

    add inputGroupHalf textbox

--- components/dashboard/inputs/textbox.jsx
+++ components/dashboard/inputs/textbox.jsx
@@ -3,19 +3,19 @@ const { inputs } = require('@data/dashboard.json');
 import styles from '@css/dashboard/textbox.module.css';
 import { useState } from 'react';
 
-export default function TextboxInput({ id, value, onChange, type }) {
+export default function TextboxInput({ id, value, onChange, type, width }) {
     const [text, setText] = useState(value);
 
     return (
-        <div className={styles.textboxcontainer}>
-            <div className={styles.textboxtext}>
-                <p className={styles.textboxheader}>{inputs[id].name}</p>
-                <p className={styles.textboxdescription}>{inputs[id].description}</p>
+        <div className={`${styles.textboxContainer} ${width == 'half' ? styles.half : null}`}>
+            <div className={`${styles.textboxText}  ${width == 'half' ? styles.half : null}`}>
+                <p className={styles.textboxHeader}>{inputs[id].name}</p>
+                <p className={styles.textboxDescription}>{inputs[id].description}</p>
             </div>
             <input
                 id={id}
                 type={type}
-                className={styles.textbox}
+                className={`${styles.textbox}  ${width == 'half' ? styles.half : null}`}
                 onChange={(e) => {
                     setText(e.target.value);
                     onChange(e);

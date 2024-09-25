77dc5428f6198b54fc430cd7900f02e1e11ff2c7
Aertic <63727339+Aertic@users.noreply.github.com>
2024-09-25 21:16:56 +0200

    rework carousel -> new controls; scss transition;  new styles and enhancements

--- components/main/carousel.jsx
+++ components/main/carousel.jsx
@@ -1,25 +1,31 @@
 'use client';
 import { useState, useEffect } from 'react';
-import styles from '@css/main/carousel.module.css';
+import styles from '@css/main/carousel.module.scss';
 import data from '@data/carousel.json';
+import Image from 'next/image';
 
 export default function Carousel({ type }) {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [manualChange, setManualChange] = useState(false);
     const images = data[type] || [];
+    const [color, setColor] = useState(null);
 
     const nextSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-        setManualChange(true);
-    };
-
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-        setManualChange(true);
+        setCurrentIndex((prevIndex) => {
+            const nextIndex = (prevIndex + 1) % images.length;
+            const nextImageElement = document.querySelector(`.${styles.image}[value="${nextIndex}"]`);
+            const c = getAverageRGB(nextImageElement);
+            setColor(`rgb(${c.r}, ${c.g}, ${c.b})`);
+            setManualChange(true);
+            return nextIndex;
+        });
     };
 
     const goToSlide = (index) => {
         setCurrentIndex(index);
+        const selectedImageElement = document.querySelector(`.${styles.image}[value="${index}"]`);
+        const c = getAverageRGB(selectedImageElement);
+        setColor(`rgb(${c.r}, ${c.g}, ${c.b})`);
         setManualChange(true);
     };
 
@@ -38,34 +44,79 @@ export default function Carousel({ type }) {
     }, [manualChange]);
 
     return (
-        <div className={styles.carousel}>
-            <div className={styles.carouselcontainer}>
-                {images.map((image, index) => (
-                    <img
-                        key={index}
-                        src={image}
-                        alt={`Slide ${index + 1}`}
-                        className={`${styles.carouselimage} ${index === currentIndex ? styles.active : ''}`}
-                    />
-                ))}
-            </div>
-            <div className={styles.controls}>
-                <button onClick={prevSlide} className={`${styles.arrow} ${styles.leftarrow}`}>
-                    &lt;
-                </button>
-                <div className={styles.dots}>
-                    {images.map((_, index) => (
-                        <span
+        <div className={styles.container} style={{ boxShadow: `0 0 50px ${color}` }}>
+            <div className={styles.innerContainer}>
+                <div className={styles.controls}>
+                    {images.map((image, index) => (
+                        <div
+                            className={`${styles.item} ${currentIndex == index ? styles.active : null}`}
                             key={index}
-                            className={`${styles.dot} ${index === currentIndex ? styles.activedot : ''}`}
+                            value={index}
                             onClick={() => goToSlide(index)}
-                        ></span>
+                        >
+                            <p className={styles.title}>{image.title}</p>
+                            <p className={styles.description}>{image.description}</p>
+                        </div>
                     ))}
                 </div>
-                <button onClick={nextSlide} className={`${styles.arrow} ${styles.rightarrow}`}>
-                    &gt;
-                </button>
+                <div className={styles.carousel}>
+                    {images.map((image, index) => (
+                        <Image
+                            width={960}
+                            height={540}
+                            key={index}
+                            src={image.url}
+                            alt={`Slide ${index + 1}`}
+                            className={`${styles.image} ${index === currentIndex ? styles.active : ''}`}
+                            value={index}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
+}
+
+function getAverageRGB(imgEl) {
+    var blockSize = 5,
+        defaultRGB = { r: 0, g: 0, b: 0 },
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data,
+        width,
+        height,
+        i = -4,
+        length,
+        rgb = { r: 0, g: 0, b: 0 },
+        count = 0;
+
+    if (!context) {
+        return defaultRGB;
+    }
+
+    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+
+    context.drawImage(imgEl, 0, 0);
+
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch (e) {
+        return defaultRGB;
+    }
+
+    length = data.data.length;
+
+    while ((i += blockSize * 4) < length) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i + 1];
+        rgb.b += data.data[i + 2];
+    }
+
+    rgb.r = ~~(rgb.r / count);
+    rgb.g = ~~(rgb.g / count);
+    rgb.b = ~~(rgb.b / count);
+
+    return rgb;
 }

ac8eedbba97486c9306be9b97abd97ddd0161572
Aertic <63727339+Aertic@users.noreply.github.com>
2024-09-26 21:27:17 +0200

    Add box shadow; remove useless css; add placeholder status grid

--- app/status/page.jsx
+++ app/status/page.jsx
@@ -16,6 +16,7 @@ export default function Page() {
                 </div>
                 <div className={styles.statusgrid}>
                     <GridHeader />
+                    <StatusGrid />
                 </div>
             </div>
         </>
